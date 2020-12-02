f4bd8e85e6b0f5a3db0c93f122a935aec6e0a1aa
Cade McPartlin <cademcp@gmail.com>
2020-12-02 10:04:36 -0600

    Add question and examples to each answer.

--- DAY2-Q1.py
+++ DAY2-Q1.py
@@ -1,3 +1,11 @@
+# Each line gives the password policy and then the password.
+# The password policy indicates the lowest and highest number of times a given letter
+# must appear for the password to be valid. For example, '1-3 a' means that the password
+# must contain 'a' at least 1 time and at most 3 times.
+# Example:
+# 1-3 a: abcde  -- valid
+# 1-3 b: cdefg  -- not valid
+# 2-9 c: ccccccccc  -- valid
 def findValidPasswords():
     validPasswords = 0
 
--- DAY2-Q2.py
+++ DAY2-Q2.py
@@ -1,3 +1,12 @@
+# Each policy actually describes two positions in the password,
+# where 1 means the first character, 2 means the second character, and so on.
+# (Be careful; Toboggan Corporate Policies have no concept of "index zero"!)
+# Exactly one of these positions must contain the given letter.
+# Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+# Example
+# 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+# 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+# 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
 def findValidPasswords():
     validPasswords = 0
 
