0070bde47520662034d28c1049cae5de348c3eb8
Cade McPartlin <cademcp@gmail.com>
2020-12-06 14:03:53 -0600

    Complete Day 6 Q2.

--- DAY6-Q1.py
+++ DAY6-Q1.py
@@ -1,30 +1,67 @@
+# https://adventofcode.com/2020/day/6
 def findQuestionCount():
 
     f = open('day6-input.txt', 'r')
 
-
-    questionsAnsweredYes = set()
-    totalQuestionsAnsweredYes = 0
+    anyQuestionsAnsweredYes = set()
+    totalAnyQuestionsAnsweredYes = 0
+    totalAllQuestionsAnsweredYes = 0
+    allQuestionsAnsweredYes = list()
+    personInGroupCount = 0
     for line in f:
 
         # New line signals end of group, so determine questions they answered yes to
         if line == '\n':
-            # print('Questions answered yes: ' + str(questionsAnsweredYes))
-            print('Count questions answered yes: ' + str(len(questionsAnsweredYes)))
-            totalQuestionsAnsweredYes += len(questionsAnsweredYes)
-            questionsAnsweredYes = set()
+            # print('Questions answered yes: ' + str(anyQuestionsAnsweredYes))
+            # print('Count questions answered yes: ' + str(len(anyQuestionsAnsweredYes)))
+
+            # print('All questions answered yes: ' + str(allQuestionsAnsweredYes))
+
+            # list of all possible questions
+            questionList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+            # loop over possible questions
+            for letter in questionList:
+
+                # get count of how many times question appears in list of questions answered yes
+                letterCount = allQuestionsAnsweredYes.count(letter)
+
+                # if the question count matches how many people are in the group,
+                # add 1 to all questions answered yes
+                if letterCount == personInGroupCount:
+                    totalAllQuestionsAnsweredYes += 1
+
+            # The total of any questions answered yes is the length of the set
+            # since sets are unique values only
+            totalAnyQuestionsAnsweredYes += len(anyQuestionsAnsweredYes)
+
+            # reset set for next run
+            anyQuestionsAnsweredYes = set()
+
+            # reset list for next run
+            allQuestionsAnsweredYes = list()
+
+            # reset count for next run
+            personInGroupCount = 0
         else:
 
             line = line.rstrip('\n')
-            # print(line)
 
+            # Q2 set
+            personInGroupCount += 1
             for i in line:
-                questionsAnsweredYes.add(i)
+                allQuestionsAnsweredYes.append(i)
+
+            # Q1 set
+            for i in line:
+                anyQuestionsAnsweredYes.add(i)
 
 
     f.close()
 
-    print('Total questions answered yes: ' + str(totalQuestionsAnsweredYes))
+    print('Total ANY questions answered yes: ' + str(totalAnyQuestionsAnsweredYes))
+    print('Total ALL questions answered yes: ' + str(totalAllQuestionsAnsweredYes))
 
 if __name__ == '__main__':
     findQuestionCount()
