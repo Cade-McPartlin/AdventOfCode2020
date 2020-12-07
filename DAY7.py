d899f014dcf0eae7029cb15f609df37b939eb818
Cade McPartlin <cademcp@gmail.com>
2020-12-07 15:11:53 -0600

    Add comments.

--- DAY7.py
+++ DAY7.py
@@ -5,11 +5,7 @@ def countShinyGoldBags(lines, bagName, masterContainers, containerBags):
 
     for line in lines:
         if bagName in line:
-
-            bagIndex = line.find(bagName)
-
             containerBag = ' '
-
             # get container bag from beginning of line
             containerBag = containerBag.join(line.split()[:3])
 
@@ -19,16 +15,24 @@ def countShinyGoldBags(lines, bagName, masterContainers, containerBags):
                 containerBag = containerBag[:-1]
 
             if containerBag != 'shiny gold bag':
+                # Add bag to container bag for future searches
                 containerBags.add(containerBag)
+                # Add bag to master container since it can hold a shiny gold bag
                 masterContainers.add(containerBag)
+                # add line to linesToRemove
                 linesToRemove.append(line)
 
-    # Remove lines
+    # Remove lines to create a new subset to search through
     for line in linesToRemove:
         lines.remove(line)
 
+    # once there are no more bags to search for, return the master container
     if len(containerBags) == 0:
         return masterContainers
+
+    # Call method recursively, pass in new subset of line,
+    # bag name, master container of bags that hold shiny gold bag,
+    # and new subset of container bags
     countShinyGoldBags(lines, containerBags.pop(), masterContainers, containerBags)
 
 
